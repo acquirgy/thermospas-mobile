ab4535520ff9b8989f930d22737ec3754a7e1dec
Amy Langa <amy@bythepixel.com>
2014-05-01 12:31:59 -0600

    Styling tweaks; Add terms and conditions.

---
+++ terms-and-conditions.php
@@ -0,0 +1,19 @@
+<?php include('includes/header.php') ?>
+
+<h1>Terms and Conditions</h1>
+
+<h2>$1,000 Bonus Gifts Breakdown*</h2>
+
+<div class='breakdown'>
+
+  <p><strong>$400 Cash Discount</strong> - Save $400 on any Designer Series Spa</p>
+
+  <p><strong>Free Delivery &amp; Installation</strong> - A $450 value that includes a full inspection and complete spa education</p>
+
+  <p><strong>Free Chemical Starter Kit</strong> - A $150 value that includes everything you need to start enjoying your hot tub immediately</p>
+
+  <p><i>*$1,000 Bonus Gifts are only valid during initial visit and may not be combined with any other offer. Discount only applies to the purchase of a Designer Series hot tub.</i></p>
+
+</div>
+
+<?php include('includes/footer.php') ?>
