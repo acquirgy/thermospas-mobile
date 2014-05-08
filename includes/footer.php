7270eb371393507b9af7c817667305d724e78580
Roeland Reyniers <roeland@bythepixel.com>
2014-05-08 08:44:10 -0600

    Fix dekstop site link.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
             <div class="footer">
-              <a href="http://wwww.thermospas.com" class="desktop-site">View Desktop Site</a> |
+              <a href="http://www.thermospas.com/" class="desktop-site">View Desktop Site</a> |
               <a href="http://www.thermospas.com/privacy-policy.html">Privacy Policy</a> |
               <a href="/terms-and-conditions.php">Terms and Conditions</a>
               <p>&copy;2014 Thermospas Hot Tub Products, Inc. All rights reserved.</p>
